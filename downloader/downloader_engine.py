1f15c0a325bfa2566ad82779e20f933df5f31408
bob1ch <bobich1488@mail.ru>
2024-03-17 14:49:17 +1000

    some changes in downloader module

--- downloader/downloader_engine.py
+++ downloader/downloader_engine.py
@@ -2,12 +2,12 @@ from pytube import YouTube
 
 VIDEO_SAVE_DIRECTORY = "./videos"
 
-def download_video(video_url):
+def download_video(video_url, filename):
     video = YouTube(video_url)
     video = video.streams.get_highest_resolution()
 
     try:
-        video.download(VIDEO_SAVE_DIRECTORY)
+        video.download(VIDEO_SAVE_DIRECTORY, filename=filename)
     except:
         print("Failed to downlaod video")
 
--- main.py
+++ main.py
@@ -7,5 +7,5 @@ from yolo import yolo_tracking as model
 
 
 if __name__ == "__main__":
-    d_eng.download_video("https://youtu.be/GnRQtA28BfY?si=8-_PZs_wWJU7HHPL")
+    d_eng.download_video("https://youtu.be/GnRQtA28BfY?si=8-_PZs_wWJU7HHPL", 'test.mp4')
     model.track()
