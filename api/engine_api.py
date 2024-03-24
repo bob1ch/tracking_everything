2c754540d546bb4f1e5e5e420745d43196f8b86b
bob1ch <bobich1488@mail.ru>
2024-03-24 17:52:38 +1000

    added downloader endpoint

--- api/engine_api.py
+++ api/engine_api.py
@@ -1,7 +1,17 @@
+import uvicorn
 from fastapi import FastAPI
+from downloader import downloader_engine as d_eng
 
 app = FastAPI()
 
 @app.get('/')
 async def test():
     return {"hello": "world"}
+
+@app.get('/download')
+def download(url: str = "https://youtu.be/GnRQtA28BfY?si=8-_PZs_wWJU7HHPL", 
+             name: str = "test.mp4"):
+    d_eng.download_video(url, name)
+
+
+uvicorn.run(app, host="0.0.0.0", port=8000)
--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from downloader import downloader_engine as d_eng
 from yolo import yolo_tracking as model 
-
-
-
+from api import engine_api
 
 
 
 if __name__ == "__main__":
+    engine_api
     d_eng.download_video("https://youtu.be/GnRQtA28BfY?si=8-_PZs_wWJU7HHPL", 'test.mp4')
-    model.track()
+    #model.track()
